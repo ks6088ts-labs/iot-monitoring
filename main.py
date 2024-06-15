c7d6e288b98bbb4b3a31402dc59a86942a38a1e8
ks6088ts <ks6088ts@gmail.com>
2024-06-16 07:31:30 +0900

    add capture image feature

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from typing import Annotated
 import typer
 from dotenv import load_dotenv
 
-from cmds.iothub.receive_direct_method import receive_direct_method as iothub_receive_direct_method
-from cmds.iothub.send_message import send_message as iothub_send_message
-from cmds.iothub.upload_to_blob import upload_to_blob as iothub_upload_to_blob
-from cmds.prometheus.simulator import run_simulators
-
 app = typer.Typer()
 
 
@@ -29,6 +24,8 @@ def setup_logging(debug: bool = False):
 def send_message(
     debug: Annotated[bool, typer.Option(help="Enable debug mode")] = False,
 ):
+    from cmds.iothub.send_message import send_message as iothub_send_message
+
     setup_logging(debug)
     asyncio.run(iothub_send_message())
 
@@ -37,6 +34,8 @@ def send_message(
 def receive_direct_method(
     debug: Annotated[bool, typer.Option(help="Enable debug mode")] = False,
 ):
+    from cmds.iothub.receive_direct_method import receive_direct_method as iothub_receive_direct_method
+
     setup_logging(debug)
     asyncio.run(iothub_receive_direct_method())
 
@@ -46,6 +45,8 @@ def upload_to_blob(
     blob_name: Annotated[str, typer.Option(help="Enable debug mode")] = "YourBlobName",
     debug: Annotated[bool, typer.Option(help="Enable debug mode")] = False,
 ):
+    from cmds.iothub.upload_to_blob import upload_to_blob as iothub_upload_to_blob
+
     setup_logging(debug)
     asyncio.run(iothub_upload_to_blob(blob_name=blob_name))
 
@@ -56,11 +57,29 @@ def run_prometheus_target(
     port: Annotated[int, typer.Option(help="Port to run the Prometheus target on")] = 8000,
     debug: Annotated[bool, typer.Option(help="Enable debug mode")] = False,
 ):
+    from cmds.prometheus.simulator import run_simulators
+
     setup_logging(debug)
     run_simulators(
         num_devices=num_devices,
         port=port,
     )
+
+
+@app.command()
+def capture_image(
+    camera_index: Annotated[int, typer.Option(help="Camera index to capture image from")] = 0,
+    path: Annotated[str, typer.Option(help="Path to save the image")] = "image.jpg",
+    debug: Annotated[bool, typer.Option(help="Enable debug mode")] = False,
+):
+    from cmds.cv.capture import capture_image as cv_capture_image
+
+    setup_logging(debug)
+    data = cv_capture_image(
+        camera_index=camera_index,
+    )
+    with open(path, "wb") as f:
+        f.write(data)
 
 
 if __name__ == "__main__":
